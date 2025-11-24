41f8ba139b8a92c359036377bf6ef6605e77de0d
hotelinminsk <yahya.kuyumcu@gmail.com>
2025-11-24 22:34:09 +0300

    testfix

--- lib/commons/commons.cpp
+++ lib/commons/commons.cpp
@@ -1,21 +1,32 @@
 #include "commons.hpp"
+#include <sys/stat.h>
 
 namespace shell_commons {
-    std::string getSystemName(){
-    #ifdef _WIN32
-        return "Windows";
-    #elif __linux__
-        return "Linux";
-    #elif __APPLE__
-        return "macOS";
-    #else
-        return "Unknown";
-    #endif // DEBUG
+
+std::string getSystemName() {
+#ifdef _WIN32
+    return "Windows";
+#elif __linux__
+    return "Linux";
+#elif __APPLE__
+    return "macOS";
+#else
+    return "Unknown";
+#endif
 }
 
 std::string trim(const std::string& s){
     auto start = std::find_if_not(s.begin(), s.end(), ::isspace);
-    auto end = std::find_if_not(s.rbegin(), s.rend(), ::isspace).base();
-    return (start < end ? std::string(start,end) : std::string());
+    auto end   = std::find_if_not(s.rbegin(), s.rend(), ::isspace).base();
+    return (start < end ? std::string(start, end) : std::string());
 }
+
+bool directoryExists(const std::string& path){
+    struct stat info;
+    if (stat(path.c_str(), &info) != 0) {
+        return false;
+    }
+    return (info.st_mode & S_IFDIR) != 0;
 }
+
+} // namespace shell_commons
